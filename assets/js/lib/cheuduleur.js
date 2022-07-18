5491259e6e2532c56fcf2dab2212a584272052ba
corentinmagique <corentinckf.pro@gmail.com>
2022-07-18 17:11:05 +0400

    date

--- assets/js/lib/cheuduleur.js
+++ assets/js/lib/cheuduleur.js
@@ -4,83 +4,55 @@ class Cheuduleur
 
         const defaultOptions = {
             divParentSelector: '.cheuduleur',
-            rows: [],
-            days: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'],
-            daysShort: ['Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.', 'Dim.'],
+            days: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+            daysShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
             serviceName: 'Chambres',
             events: []
         }
     
         this.options = {...defaultOptions, ...opts}
         this.divParentElement = document.querySelector(this.options.divParentSelector)
-        this.eventsMatrix = Array(this.options.rows.length).fill().map(()=>Array(this.options.days.length).fill())
-    }
-
-    init(){
-
-        this.options.rows.forEach((row, n) => {
-            let eventsObjects = this.options.events[row]
-            let keys = Object.keys(this.options.events)
-
-            this.options.days.forEach((day, index) => {
-
-                if(eventsObjects)
-                {
-                    eventsObjects.forEach(event => {
-                        if(index >= event.startDate && index <= event.endDate && n == keys.indexOf(row))
-                            this.eventsMatrix[n][index] = event.eventId
-                        
-                    })
-                }
-                
-            })
-            
-        })
-
-        console.log(this.eventsMatrix)
-
     }
 
     draw(){
         let tableHeader = `<th>${this.options.serviceName}</th>`
-        let tableBody = ``, events = ``
+        let tableBody = ``
 
         this.divParentElement.classList.add('cheuduleur-container')
         
-        this.options.days.forEach(day => {
-            tableHeader += `<th>${day}</th>`
-        })
+        for(let i = 0; i <= this.daysNumber();++i)
+        {
+            // console.log(this.dayDate(i).getDay())
+            tableHeader += `<th><span class="text-secondary">${this.options.daysShort[this.dayDate(i).getDay()]}</span> <span>${this.dayDate(i).getDate()}</span></th>`
+        }
 
-        this.options.rows.forEach((row, n) => {
-            let tableElements = `<td>${row}</td>`
-            let eventElements = `<td>${row}</td>`
-            let eventsObjects = this.options.events[row]
-            let keys = Object.keys(this.options.events)
-
-            this.options.days.forEach((day, index) => {
-                let borderStyle = ``
-                let colspan = ``
-                tableElements += `<td></td>`
-                if(this.eventsMatrix[n][index]!=undefined)
+        this.options.events.forEach((event)=>{
+            let key = Object.keys(event)
+            let tableElements = `<td>${key}</td>`
+            
+            this.options.days.forEach((day, index)=>{
+                
+                let content = ``
+                if(index == 0)
                 {
-                    let eventDetail = this.eventDetails(row,this.eventsMatrix[n][index])
-                    eventElements += `<td style="background-color: ${eventDetail.backgroundColor};border-color:${eventDetail.backgroundColor};"></td>`
-                }else eventElements += `<td></td>`
-                
-                
+                    event[key].forEach((el, index)=>{
+                        let neighborhood = this.neighborhood(key, el, event)
+                        content += this.renderEvent(key, el, neighborhood)
+                    })
+                    
+                }else
+                {
+                    content = ``
+                }
+                tableElements += `<td>${content}</td>`
             })
+
             let tableRow = `
                 <tr>
                     ${tableElements}
                 </tr>
             `
-            let eventRow = `
-                <tr>
-                    ${eventElements}
-                </tr>
-            `
             tableBody += tableRow
-            events += eventRow
         })
 
         this.divParentElement.appendChild(this.stringToHtml(`
@@ -97,31 +69,62 @@ class Cheuduleur
             
         `))
 
-        this.divParentElement.appendChild(this.stringToHtml(`
-            <div>
-                <table class="cheuduleur-events">
-                    <thead>
-                        ${tableHeader}
-                    </thead>
-                    <tbody>
-                        ${events}
-                    </tbody>
-                </table>
-            </div>
-            
-        `))
-
         document.querySelectorAll('.eventDetails').forEach((event) => {
-            
             event.addEventListener('click', (e) =>{
+                let service = e.currentTarget.getAttribute('dt-service')
                 let event_id = e.currentTarget.getAttribute('dt-event-id')
-                alert(event_id)
+                alert(`${service} event n° ${event_id}`)
             })
         })
+
+        let smlh = this.options.events
+
+        console.log(Object.keys(smlh[0]))
+    }
+
+    renderEvent(key, el, neighborhood){
+
+        let widthOffset = -100
+        let leftOffset = 50
+
+        return `
+            <div dt-service="${key}" dt-event-id="${el.eventId}" class="eventDetails" style="
+            background-color:${el.backgroundColor}85;
+            border: 2px solid ${el.backgroundColor};
+            width: calc( ${(el.endDate - el.startDate + 1) * 100}% + ${widthOffset}%);
+            left: calc( ${el.startDate * 100}% + ${leftOffset}%);
+            border-radius: 0.75rem;
+            padding: 0.5rem;
+            color: ${el.backgroundColor};
+            font-weight: bold;
+            z-index: 50;
+            ">
+            ${el.username}
+            </div>
+        `
+    }
+
+    neighborhood(key, event, events)
+    {
+        return { before : (events[key][events[key].indexOf(event) - 1] && (events[key][events[key].indexOf(event) - 1].endDate == events[key][events[key].indexOf(event)].startDate)) , after : (events[key][events[key].indexOf(event) + 1] && (events[key][events[key].indexOf(event)].endDate == events[key][events[key].indexOf(event) + 1].startDate))}
+        
     }
 
     eventDetails(serviceName, eventId){
         return this.options.events[serviceName].filter((event) => event.eventId === eventId)[0]
+    }
+
+    daysNumber()
+    {
+        let endDate = new Date(this.options.endDate)
+        let startDate = new Date(this.options.startDate)
+        return Math.ceil((endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24))
+    }
+
+    dayDate(offsetDay)
+    {
+        let startDate = new Date(this.options.startDate)
+        return new Date(new Date(startDate.getTime() + (1000 * 3600 * 24 * offsetDay)))
     }
 
     stringToHtml(template){
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,69 @@
 let test = new Cheuduleur({
-    rows: ['Chambre 1', 'Chambre 2', 'Chambre 3'],
     days: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'],
     serviceName: 'Chambres',
-    events: {
-        'Chambre 1' : [
-            {
-                eventId: 0,
-                startDate: 0,
-                endDate: 3,
-                backgroundColor: 'black'
-            },
-            {
-                eventId: 1,
-                startDate: 5,
-                endDate: 7,
-                backgroundColor: 'orange'
-            }
-        ],
-        'Chambre 2' : [
-            {
-                eventId: 2,
-                startDate: 2,
-                endDate: 4,
-                backgroundColor: 'purple'
-            }
-        ],
-        'Chambre 3' : [
-            {
-                eventId: 3,
-                startDate: 4,
-                endDate: 6,
-                backgroundColor: 'green'
-            }
-        ]
-    } 
+    startDate : '07/25/2022',
+    endDate : '07/31/2022',
+    events: [
+        {
+            'Chambre 1' : [
+                {
+                    eventId: 0,
+                    startDate: 1,
+                    endDate: 2,
+                    backgroundColor: '#1abc9c',
+                    username: 'Jean'
+                },
+                {
+                    eventId: 1,
+                    startDate: 2,
+                    endDate: 5,
+                    backgroundColor: '#2ecc71',
+                    username: 'Jean'
+                }
+            ]
+        },
+        {
+            'Chambre 2' : [
+                {
+                    eventId: 2,
+                    startDate: 0,
+                    endDate: 1,
+                    backgroundColor: '#3498db',
+                    username: 'Jean'
+                },
+                {
+                    eventId: 3,
+                    startDate: 1,
+                    endDate: 4,
+                    backgroundColor: '#9b59b6',
+                    username: 'Jean'
+                }
+                ,
+                {
+                    eventId: 3,
+                    startDate: 5,
+                    endDate: 6,
+                    backgroundColor: '#34495e',
+                    username: 'Jean'
+                }
+               
+            ]
+        },
+        {
+            'Chambre 3' : [
+                {
+                    eventId: 4,
+                    startDate: 1,
+                    endDate: 6,
+                    backgroundColor: '#e74c3c',
+                    username: 'Jean'
+                }
+            ]
+        }
+        
+        
+    ]
         
 })
 
-test.init()
 test.draw()
